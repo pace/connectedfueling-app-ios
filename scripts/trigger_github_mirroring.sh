8fec3b5961563d3565b90b6eb67153855afd4a9e
Patrick Niepel <patrick@pace.car>
2023-10-23 11:05:15 +0200

    Fix mirroring script

--- scripts/trigger_github_mirroring.sh
+++ scripts/trigger_github_mirroring.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git remote set-url origin "git@$CI_SERVER_HOST:$CI_PROJECT_PATH.git" # Set correct git url
-git fetch --all && git pull --rebase # Update main if needed
+git fetch --all
+git checkout main && git pull --rebase # Update main if needed
 git checkout release && git pull --rebase # Update release branch if needed
 git merge main && git push # Merge main into release
